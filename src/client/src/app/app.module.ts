e0582f23b7b71e0090ce92c204592021ed448ee1
AndreyShepeta <andrey.shepeta@gmail.com>
2018-07-20 15:20:48 +0300

    created some components

--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { InfoSidebarComponent } from './info-sidebar/info-sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    InfoSidebarComponent
   ],
   imports: [
     BrowserModule
