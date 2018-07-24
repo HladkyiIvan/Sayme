aeb21fb6317df6738a5ca46ffb46b726fd0d8977
AndreyShepeta <andrey.shepeta@gmail.com>
2018-07-24 14:22:01 +0300

    last chance

--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { RegistrationModule } from './modules/registration/registration.module';
 import { UserProfileModule } from './modules/user-profile/user-profile.module';
 import { AnyProfileModule } from './modules/any-profile/any-profile.module';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TestComponent,
   ],
   imports: [
     BrowserModule,
