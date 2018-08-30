9a08a933260cb3589c66c8e5da6dda33237b4b77
Ivan Hladkiy <liberte1793@gmail.com>
2018-08-31 00:28:18 +0300

    host fix 4

--- src/server/Program.cs
+++ src/server/Program.cs
@@ -14,7 +14,15 @@ namespace server
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = new WebHostBuilder()
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .UseIISIntegration()
+            .UseStartup<Startup>()
+            .UseUrls("http://localhost:5001/")
+            .Build();
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
