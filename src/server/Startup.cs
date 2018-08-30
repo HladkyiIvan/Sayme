c279920b690ff1fa1454228eb35ca131468a9910
Ivan Hladkiy <liberte1793@gmail.com>
2018-08-30 23:39:30 +0300

    host fix

--- src/server/Startup.cs
+++ src/server/Startup.cs
@@ -34,7 +34,7 @@ namespace server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContextPool<SaymedbContext>(options => options.UseMySql(connection);
+            services.AddDbContextPool<SaymedbContext>(options => options.UseMySql(connection));
 
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
